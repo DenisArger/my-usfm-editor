cd6332e6224dec949b69ce8ca1308e8916c49c4e
Denis Arger <den.arger@gmail.com>
2024-02-02 12:26:29 +0300

    build: first build

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ShowUsfmEditor from "./ShowUsfmEditor.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ShowUsfmEditor />
+  </React.StrictMode>
+);
