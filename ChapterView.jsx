d7ecb24eb2fcd435038fc9b300315bac152dca5c
Denis Arger <den.arger@gmail.com>
2024-02-06 16:58:58 +0300

    feat: add cach proskomma

--- ChapterView.jsx
+++ ChapterView.jsx
@@ -4,25 +4,37 @@ import axios from "axios";
 
 function ChapterView({ url, chapterNumber }) {
   const [versesData, setVersesData] = useState([]);
+  const [pk, setPk] = useState(new Proskomma());
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const pk = new Proskomma();
+        const newPk = new Proskomma();
         const data = await axios.get(url).then((response) => response.data);
-        pk.importDocument({ abbr: "rlob", lang: "rus" }, "usfm", data);
+        newPk.importDocument({ abbr: "rlob", lang: "rus" }, "usfm", data);
+        setPk(newPk);
+      } catch (error) {
+        console.error("An error occurred while importing document:", error);
+      }
+    };
 
+    fetchData();
+  }, [url]);
+
+  useEffect(() => {
+    const fetchVerses = async () => {
+      try {
         const res = await pk.gqlQuerySync(`{
           documents {
-              cvIndex(chapter:${chapterNumber}) {
-                 verses {
-                    verse {
-                       verseRange
-                       text
-                    }
-                 }
+            cvIndex(chapter:${chapterNumber}) {
+              verses {
+                verse {
+                  verseRange
+                  text
+                }
               }
-           }
+            }
+          }
         }`);
 
         const versesArray = res.data.documents[0].cvIndex.verses.map(
@@ -35,12 +47,12 @@ function ChapterView({ url, chapterNumber }) {
         setVersesData(versesArray);
         console.log(versesArray);
       } catch (error) {
-        console.error("An error occurred:", error);
+        console.error("An error occurred while fetching verses:", error);
       }
     };
 
-    fetchData();
-  }, [url, chapterNumber]);
+    fetchVerses();
+  }, [pk, chapterNumber]);
 
   return (
     <div>
